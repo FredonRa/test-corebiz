015b7af4e79da230587ee9d7433d64a823961a82
FredonRa <fede_99_leiva@hotmail.com>
2022-05-06 15:42:13 -0300

    fix actions localstorage

--- src/actions/cart.actions.ts
+++ src/actions/cart.actions.ts
@@ -3,7 +3,7 @@ import { Product } from '../types/product';
 
 // Get all products from local storage
 export const setStorageProducts = () => {
-    let products = JSON.parse(localStorage.getItem('products') || '{}');
+    let products = JSON.parse(localStorage.getItem('products') || '[]');
     return {
         type: t.SET_ALL_PRODUCT,
         payload: products??=[],
@@ -12,7 +12,7 @@ export const setStorageProducts = () => {
 
 // Receive product for add in the cart
 export const setAddProduct = (product: Product) => {
-    let products = JSON.parse(localStorage.getItem('products') || '{}');
+    let products = JSON.parse(localStorage.getItem('products') || '[]');
     if (!products || products == null || product == undefined) products = [];
     let newProducts = [...products, product];
     localStorage.setItem('products', JSON.stringify(newProducts));
@@ -24,7 +24,7 @@ export const setAddProduct = (product: Product) => {
 
 // Receive product for remove him in the cart
 export const setRemoveProduct = (product: Product) => {
-    let products = JSON.parse(localStorage.getItem('products') || '{}');
+    let products = JSON.parse(localStorage.getItem('products') || '[]');
     let cont = 0;
     const indexOfElement = products.filter((prod: Product) => {
         if (cont) return prod;
